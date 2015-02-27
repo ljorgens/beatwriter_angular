3fad29bf5e9ee71f8f4aac62a1735254c2f01bbc
ljorgens <leejorgensen22@gmail.com>
2015-02-27 11:09:40 -0800

    add new reporters

--- controllers/ReporterCtrl.js
+++ controllers/ReporterCtrl.js
@@ -1,3 +1,8 @@
 beatTracker.controller('ReporterCtrl', function ReporterCtrl($scope, $stateParams, BeatFactory, UtilityFactory){
 	$scope.beat = UtilityFactory.findById(BeatFactory.beats, $stateParams.beatId);
+	
+	$scope.assignReporter = function(){
+		$scope.beat.reporters.push({ name: $scope.reporterName, birthday: $scope.addBirthday, employment: $scope.addEmployment});
+		$scope.reporterName = null;
+	}
 });
